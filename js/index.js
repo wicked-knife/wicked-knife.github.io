6c07c5a91c0c97dde908aa90990bc2ceb5b753cf
ghostratel <1353640486@qq.com>
2018-01-31 13:52:55 +0800

    add eleme app

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ $(function(){
         var linkArr = ["portfolio/Todo list/index.html",
                         "portfolio/RandomQuoteMachine/index.html",
                         "portfolio/Wikipedia Viewer/index.html",
-                        "portfolio/LocalWeather/index.html"];
+                        "portfolio/LocalWeather/index.html",
+                        "portfolio/eleme/index.html"];
         for(let i = 0;i<linkArr.length;i++) {
             $(".future_ui__piece").eq(i).on("click",function () {
                 window.open(linkArr[i],"_blank");
